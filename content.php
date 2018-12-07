fd71dcb39e8da24e9f93f5169cad33130345680f
stephane00 <38441365+stephane00@users.noreply.github.com>
2018-12-07 02:42:35 +0100

    file
    create file

---
+++ content.php
@@ -0,0 +1,2 @@
+  <body>
+    <p>Bonjour!!!</p>
---
+++ header.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>Ma Table</title>
+    <link rel="shortcut icon" href="ici le nom de ton favicon.ico">
+    <link rel="stylesheet" href="style.css">
+  </head>
