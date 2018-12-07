a9946c90174e4c6de1e25fcc2ea2b1d0c6db2435
stephane00 <38441365+stephane00@users.noreply.github.com>
2018-12-07 03:33:52 +0100

    Add files via upload

--- content.php
+++ content.php
@@ -1,11 +1,11 @@
   <body>
-    <div class="Content">
-      <div class="Restaurateur">
+    <div class="content">
+      <div class="restaurateur">
         <h1>Restaurateur<h1>
         <p>blabla<p>
         <button type="button" class="btn">Découvrir</button>
       </div>
-      <div class="Client">
+      <div class="client">
         <h1>Client</h1>
         <p>blabla</p>
         <button type="button" class="btn">Découvrir</button>
