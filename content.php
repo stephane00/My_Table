29b347533c652917b001d35ad756b4b4098aec03
stephane00 <38441365+stephane00@users.noreply.github.com>
2018-12-07 03:09:55 +0100

    style

--- content.php
+++ content.php
@@ -1,2 +1,13 @@
   <body>
-    <p>Bonjour!!!</p>
+    <div class="Content">
+      <div class="Restaurateur">
+        <h1>Restaurateur<h1>
+        <p>blabla<p>
+        <button type="button" class="btn">Découvrir</button>
+      </div>
+      <div class="Client">
+        <h1>Client</h1>
+        <p>blabla</p>
+        <button type="button" class="btn">Découvrir</button>
+      </div>
+    </div>
